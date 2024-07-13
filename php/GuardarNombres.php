98c6ac5559ec3e825b0c640d58c56789ed58a450
Brandon <brandonde2002@gmail.com>
2024-07-12 21:49:32 -0600

    ldld

--- php/GuardarNombres.php
+++ php/GuardarNombres.php
@@ -1,3 +1,8 @@
 <?php
-phpinfo();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    echo "Parámetros POST:<br>";
+    foreach ($_POST as $key => $value) {
+        echo "$key: $value<br>";
+    }
+}
 ?>
