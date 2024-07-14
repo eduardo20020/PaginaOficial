3b877cd68414abb42db5a72c64ef029aec2adb34
Brandon <brandonde2002@gmail.com>
2024-07-13 22:56:28 -0600

    ldld

--- php/GuardarNombres.php
+++ php/GuardarNombres.php
@@ -1,5 +1,19 @@
 <?php
 
-echo file_get_contents('php://input');
+$json = file_get_contents('php://input');
 
+$data = json_decode($json,true);
+
+if($data===null){
+    http_response_code(400);
+    echo json_encode(['error' => 'JSON inválido']);
+    exit;
+}
+
+$nombre = $data['Nombre'];
+
+$password = $data['Contra'];
+
+
+echo "Tu nombre es: ". $nombre . " y tu contra es: puchaina";
 ?>
